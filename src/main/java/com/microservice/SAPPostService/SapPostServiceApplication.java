9594c42c431fe2ccac43e1e48617fe35b74ec827
Firebase Studio <noreply@firebase.studio>
2025-08-31 09:47:04 +0000

    Initialized workspace with Firebase Studio

---
+++ src/main/java/com/microservice/SAPPostService/SapPostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.SAPPostService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapPostServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/SAPPostService/SapPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.SAPPostService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
