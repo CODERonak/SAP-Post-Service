c469a2b30c24c741bb925ed87140f2d834525613
r9988.2588 <r9988.2588@gmail.com>
2025-09-02 03:45:05 +0000

    added model, repository and dto's

---
+++ src/main/java/com/microservice/SAPPostService/dto/PostRequest.java
@@ -0,0 +1,25 @@
+package com.microservice.SAPPostService.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+/*
+ * This is the DTO class for the posts table in the database.
+ * It has the following fields:
+ * - authorId: author id a foreign key to the users table
+ * - title: the title of the post
+ * - content: the content of the post
+ */
+
+@Data
+public class PostRequest {
+    @NotNull
+    private Long authorId;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+}
---
+++ src/main/java/com/microservice/SAPPostService/repository/PostRepository.java
@@ -0,0 +1,16 @@
+package com.microservice.SAPPostService.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.microservice.SAPPostService.model.Post;
+
+/*
+ * This is the repository interface for the users table in the database.
+ * It has the folowing methods:
+ * - existsByAuthorId: check if a user exists by authorId: find a user by email
+ */
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    boolean existsByAuthorId(Long authorId);
+}
