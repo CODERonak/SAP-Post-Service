41bf5cc6821d3c3640623e803d995e458ee8c013
r9988.2588 <r9988.2588@gmail.com>
2025-09-03 03:43:13 +0000

    added a method

--- src/main/java/com/microservice/SAPPostService/repository/PostRepository.java
+++ src/main/java/com/microservice/SAPPostService/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.microservice.SAPPostService.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +10,12 @@ import com.microservice.SAPPostService.model.Post;
 /*
  * This is the repository interface for the users table in the database.
  * It has the folowing methods:
- * - existsByAuthorId: check if a user exists by authorId: find a user by email
+ * - existsByAuthorId: check if a user exists by authorId
+ * - List findByUserId: find all posts by userId
  */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     boolean existsByUserId(Long userId);
+
+    List<Post> findByUserId(Long userId);
 }
